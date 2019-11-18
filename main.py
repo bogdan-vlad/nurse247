178ba364f9b59f81cc4a1ba7d476c387f52dd8b0
Bogdan <bogdan.vlad@tuware.com>
2019-11-18 09:36:27 +0000

    Added more pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
+
+@app.route('/work_with_us')
+def work_with_us():
+    return render_template('work_with_us.html', title='Work With Us')
 
 if __name__ == "__main__":
     app.run(debug=True)
