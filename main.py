5ca26d20a3a42f7ced6141f777bff18c074c61d9
Bogdan <bogdan.vlad@tuware.com>
2020-04-27 09:08:57 +0100

    Added sendinblue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from collections import namedtuple
 
 from flask import (
@@ -26,17 +27,55 @@ def hello():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact Us')
+
 
 @app.route('/work_with_us')
 def work_with_us():
     return render_template('work_with_us.html', title='Work With Us')
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='About')
+    return render_template('register.html')
+
+
+@app.route('/register_new_form', methods=['GET', 'POST'])
+def register_new_form():
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    telephone = request.form['telephone']
+    email = request.form['email']
+    tpe = request.form['tpe']
+
+    url = "https://api.sendinblue.com/v3/smtp/email"
+
+    payload = "{\"sender\":{\"email\":\"bogdan.vlad.@gmail.com\",\"name\":\"Bogdan Vlad\"},\"to\":[{\"email\":\"bogdan.vlad@tuware.com\",\"name\":\"Bogdan Vlad\"}],\"replyTo\":{\"email\":\"bogdan.vlad@tuware.com\"},\"subject\":\"Hello\",\"htmlContent\":\"%s %s\"}" % (first_name, last_name)
+
+    headers = {
+        'accept': "application/json",
+        'content-type': "application/json",
+        'api-key': "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"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    statement = 'INSERT INTO test (' \
+                'first_name,' \
+                'last_name,' \
+                'telephone,' \
+                'email, tpe) VALUES (?, ?, ?, ?, ?)'
+    cursor.execute(statement, (first_name, last_name, telephone, email, tpe))
+    conn.commit()
+    conn.close()
+    return render_template('index.html', response=response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,22 @@
-function display_box_conviction() {
+  function display_box_conviction() {
     let checkBox = document.getElementById("convicted_yes");
-    let text = document.getElementById("text");
+    let criminal = document.getElementById("criminal");
       if (checkBox.id == 'convicted_yes') {
-        text.style.display = "block";
-      } else {
-        text.style.display = "none";
+        criminal.style.display = "block";
       }
   }
 
-  function hide_box_conviction() {
-    let text = document.getElementById("text");
+  function display_investigation() {
+    let suspension = document.getElementById("suspension");
+    let investigation = document.getElementById("investigation");
 
-    text.style.display = "none"
+    if (suspension.id == 'suspension') {
+      investigation.style.display = 'block'
+    }
+  }
+
+  function hide_box_conviction() {
+    let criminal = document.getElementById("criminal");
+
+    criminal.style.display = "none"
   }
