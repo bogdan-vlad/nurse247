e761d635b10f492880c92347aae17cd4dbb7ac61
Bogdan <bogdan.vlad@tuware.com>
2020-05-03 19:46:58 +0100

    Improved the registration form. Styling added.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from collections import namedtuple
 
 from flask import (
@@ -33,9 +32,9 @@ def contact():
     return render_template('contact.html', title='Contact Us')
 
 
-@app.route('/work_with_us')
-def work_with_us():
-    return render_template('work_with_us.html', title='Work With Us')
+@app.route('/confirmation')
+def confirmation():
+    return render_template('confirmation.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -45,36 +44,136 @@ def register():
 
 @app.route('/register_new_form', methods=['GET', 'POST'])
 def register_new_form():
+    global group1_provide
+    global group2_provide
+    global group3_provide
 
+    title = request.form['title']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     telephone = request.form['telephone']
     email = request.form['email']
+    nationality = request.form['nationality']
+    nino = request.form['nino']
+    address = request.form['address']
+    postcode = request.form['postcode']
+    dob = request.form['dob']
     tpe = request.form['tpe']
+    perm_uk = request.form['perm_uk']
+    nmc = request.form['nmc']
+    nmc_expiry = request.form['nmc_expiry']
+    group1 = request.form['group1']
+    group1_provide = request.form['group1_provide']
+    group2 = request.form['group2']
+    group2_provide = request.form['group2_provide']
+    group3 = request.form['group3']
+    group3_provide = request.form['group3_provide']
+    group4 = request.form['group4']
 
-    url = "https://api.sendinblue.com/v3/smtp/email"
+    if nmc and nmc_expiry is not None:
+        is_nmc = nmc
+        is_nmc_expiry = nmc_expiry
+    else:
+        is_nmc = 'N/A'
+        is_nmc_expiry = 'N/A'
 
-    payload = "{\"sender\":{\"email\":\"bogdan.vlad.@gmail.com\",\"name\":\"Bogdan Vlad\"},\"to\":[{\"email\":\"bogdan.vlad@tuware.com\",\"name\":\"Bogdan Vlad\"}],\"replyTo\":{\"email\":\"bogdan.vlad@tuware.com\"},\"subject\":\"Hello\",\"htmlContent\":\"%s %s\"}" % (first_name, last_name)
+    if group1 == "yes":
+        group1_provide = request.form['group1_provide']
+    elif group2 == "yes":
+        group2_provide = request.form['group2_provide']
+    elif group3 == "yes":
+        group3_provide = request.form['group3_provide']
 
-    headers = {
-        'accept': "application/json",
-        'content-type': "application/json",
-        'api-key': "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"
-    }
+    import smtplib
 
-    response = requests.request("POST", url, data=payload, headers=headers)
+    EMAIL = os.environ.get('EMAIL')
+    PASSWORD = os.environ.get('PASSWORD')
 
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    statement = 'INSERT INTO test (' \
-                'first_name,' \
-                'last_name,' \
-                'telephone,' \
-                'email, tpe) VALUES (?, ?, ?, ?, ?)'
-    cursor.execute(statement, (first_name, last_name, telephone, email, tpe))
-    conn.commit()
-    conn.close()
-    return render_template('index.html', response=response)
+    send_from = email
+    to = 'compliance@yournurse247.co.uk'
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    msg = MIMEMultipart()
+
+    body = """
+    <html>
+    <head></head>
+      <body>
+        <p>Title: %s</p>
+        <p>First name: %s | Last name: %s</p>
+        <p>Telephone number: %s | Email: %s</p>
+        <p>Nationality: %s | National Insurance Number: %s</p>
+        <p>Address: %s | Postcode: %s</p>
+        <p>Date of Birth: %s</p>
+        <p>Are there any restrictions to your residence withing the UK that
+        might your right to take up employment in the UK? %s</p>
+        <p>If your application is successful, would you require
+        permission to work in the UK? %s</p>
+        <p>NMC/GMC/GDC pin (if applicable): %s | Expiry date: %s</p>
+        <p>Do you have any unspent* Criminal convictions? : %s | %s</p>
+        <p>Are you / have been under / or undergoing any clinical
+        investigation, disciplinary or suspension process
+        pending or otherwise? : %s | %s</p>
+        <p>Do you have any health issues or a disability relevant
+        which may make it difficult for you to carry out functions which
+        are essential for the role you seek? : %s | %s</p>
+        <p>Please tick the appropriate box to confirm that you have read
+        and understood the above information. : <br>%s</p>
+      </body>
+    </html>
+    """ % (
+        title,
+        first_name,
+        last_name,
+        telephone,
+        email,
+        nationality,
+        nino,
+        address,
+        postcode,
+        dob,
+        tpe,
+        perm_uk,
+        is_nmc,
+        is_nmc_expiry,
+        group1,
+        group1_provide,
+        group2,
+        group2_provide,
+        group3,
+        group3_provide,
+        group4,
+    )
+    # message = MIMEText(body, 'html')
+    text = 'New Application %s %s' % (first_name, last_name)
+
+    msg['FROM'] = send_from
+    msg['TO'] = to
+    msg['Subject'] = text
+
+    msg.attach(MIMEText(body, 'html'))
+
+    try:
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.sendmail(send_from, to, msg.as_string())
+        s.quit()
+    except:
+        print('Error')
+
+    # conn = sqlite3.connect(DB_FILE)
+    # cursor = conn.cursor()
+    # statement = 'INSERT INTO test (' \
+    #             'first_name,' \
+    #             'last_name,' \
+    #             'telephone,' \
+    #             'email, tpe, dob) VALUES (?, ?, ?, ?, ?, ?)'
+    # cursor.execute(statement, (first_name, last_name, telephone, email, tpe, dob))
+    # conn.commit()
+    # conn.close()
+    return redirect('/confirmation')
 
 
 if __name__ == "__main__":
--- static/js/app.js
+++ static/js/app.js
@@ -6,17 +6,44 @@
       }
   }
 
-  function display_investigation() {
-    let suspension = document.getElementById("suspension");
-    let investigation = document.getElementById("investigation");
-
-    if (suspension.id == 'suspension') {
-      investigation.style.display = 'block'
-    }
-  }
-
   function hide_box_conviction() {
     let criminal = document.getElementById("criminal");
 
     criminal.style.display = "none"
+  }
+
+
+  function display_investigation() {
+    let suspension = document.getElementById("suspension_yes");
+    let investigation = document.getElementById("investigation");
+
+    if (suspension.id == 'suspension_yes') {
+      investigation.style.display = 'block'
+    }
+  }
+
+  function hide_display_conviction() {
+    let investigation = document.getElementById("investigation");
+
+    investigation.style.display = 'none';
+  }
+
+  function display_health_yes() {
+    let health_yes = document.getElementById("health_yes");
+    let health = document.getElementById("health");
+    if (health_yes.id == 'health_yes') {
+        health.style.display = 'block'
+    }
+  }
+
+  function hide_health_yes() {
+    let health = document.getElementById("health");
+
+    health.style.display = 'none';
+  }
+
+  function preload() {
+    let preloader = document.getElementById("preloader");
+
+    preloader.style.display = 'block';
   }
